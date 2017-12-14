4a484e46e1852245834b3986d0b332f5ce029410
liushuwei <lsw305264677@gmail.com>
2017-12-15 00:04:29 +0800

    增加反射用法

--- app/src/main/java/com/lsw/reflectiondemo/MainActivity.java
+++ app/src/main/java/com/lsw/reflectiondemo/MainActivity.java
@@ -2,12 +2,65 @@ package com.lsw.reflectiondemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.lang.reflect.Method;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getMethodNum();
+        getClassName();
+        createPerson();
     }
+
+    private void getMethodNum() {
+
+        Method[] methods = Person.class.getMethods();
+        for (Method method : methods) {
+            Log.i(TAG, "getMethodNum: " + method.getName());
+        }
+    }
+
+    private void getClassName(){
+        Class c1 = Person.class;
+        Class c2 = new Person().getClass();
+        Class c3 = null;
+        try {
+            c3 = Class.forName("com.lsw.reflectiondemo.Person");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        Log.i(TAG, "getClassName: " + c1.getName());
+        Log.i(TAG, "getClassName: " + c2.getName());
+        Log.i(TAG, "getClassName: " + c3.getName());
+    }
+
+    private void createPerson(){
+        Class c = null;
+        Person person = null;
+        try {
+            c = Class.forName("com.lsw.reflectiondemo.Person");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            person = (Person)c.newInstance();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+        person.setName("lisi");
+        Log.i(TAG, "createPerson: " + person.getName());
+    }
+
 }
